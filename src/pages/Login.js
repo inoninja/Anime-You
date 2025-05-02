5c962a879b61447f8c50ea09f69ea70d24422f5b
Alienware221 <junjunmalakas09@gmail.com>
2025-05-02 12:33:01 +0800

    New Login Layout (WIP)

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import './Login.css'; // optional external CSS
-
+import './Login.css';
 
 const Login = () => {
   return (
     <div className="login-container">
+      <img
+        src="/assets/anime-logo.png"
+        alt="Side Logo"
+        className="background-logo"
+      />
+
       <div className="login-box">
         <div className="login-left">
           <h2>LOGIN</h2>
@@ -12,23 +17,21 @@ const Login = () => {
             <input type="email" placeholder="Email" />
             <input type="password" placeholder="Password" />
             <div className="forgot">
-              <button type="button" className="forgot-password">
-                Forgot Password?
-              </button>
-            </div>
-            <button type="submit" className="sign-in">
-              SIGN IN
-            </button>
+  <button type="button" className="auth-link forgot-password">
+    Forgot Password?
+  </button>
+</div>
+            <button type="submit" className="sign-in">SIGN IN</button>
             <div className="create-account">
-              <button type="button" className="create-account-btn">
-                Create Account
-              </button>
-            </div>
+  <button type="button" className="auth-link create-account-btn">
+    Create Account
+  </button>
+</div>
           </form>
         </div>
         <div className="login-right">
-          <img src="./assets/anime-logo.png" alt="Anime&You Logo" />
-          <h3>Your Anime<br />Your Dreams</h3>
+          <img src="/assets/logo.png" alt="Logo" className="logo-image" />
+          <img src="/assets/anime-slogan.png" alt="Slogan" className="slogan-image" />
         </div>
       </div>
     </div>
